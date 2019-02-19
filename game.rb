22eec96c70fc739bda220d6fb724fd7708ef5ddf
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-19 17:48:39 +0000

    add play method to print user on screen

--- game.rb
+++ game.rb
@@ -3,3 +3,18 @@
 require_relative './player'
 require_relative './turn_manager'
 
+class Game
+
+  def initialize
+    #list of players
+    @players = [
+      Player.new('Player 1'),
+      Player.new('Player 2')
+    ]
+  end
+
+  def play
+    puts "#{@players[0].name}: What does 5 plus 3 equal?"
+  end
+
+end
