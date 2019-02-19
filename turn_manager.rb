2a1d9843c3576c5538d00d4578527ac01e1acf82
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-19 19:54:05 +0000

    add next turn and increment turn definition

--- turn_manager.rb
+++ turn_manager.rb
@@ -1,3 +1,25 @@
 # handle how we will manage the next turn when user input a response
 
 require_relative './turn'
+
+class TurnManager
+  def initialize(players) 
+    puts @players = players.dup
+    @round_number = 0
+    @current_index = 0
+  end
+
+  def next_turn
+    player = @players[current_index]
+
+    increment_turn
+
+    Turn.new(player, @round_number)
+  end
+
+  def increment_turn
+    @round_number += 1
+    @current_index = (@current_index + 1) % @players.count
+  end
+end
+
